8a69e92e987a9a70263edeec144582deb1b34a8b
jotahdev <joaomanoelskai@gmail.com>
2024-12-31 06:34:29 -0300

    Formatanmdo mensagem de senha

--- src/main/java/com/jotahdev/job_development/modules/candidate/Entities/CandidateEntity.java
+++ src/main/java/com/jotahdev/job_development/modules/candidate/Entities/CandidateEntity.java
@@ -9,17 +9,19 @@ import lombok.Data;
 
 @Data
 public class CandidateEntity {
-    
+
     private UUID id;
     private String name;
 
     @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "O nome de usuário deve conter apenas letras, números e underscores. Não é permitido o uso de espaços ou caracteres especiais, como acentos ou símbolos.")
     private String username;
-    
+
     @Email(message = "Por favor, insira um email válido.")
     private String email;
-    @Size(min = 8, max = 16, message = "A senha deve ter entre 8 e 12 caracteres.")
+
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#\\$%^&\\*])(?=\\S+$).{8,16}$", message = "A senha deve ter entre 8 e 16 caracteres, incluindo pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial.")
     private String password;
+    
     private String description;
     private String curriculum;
 
