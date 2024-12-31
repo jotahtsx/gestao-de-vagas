4ef143058ae763159f8ef471ca8f103ee13b03ba
jotahdev <joaomanoelskai@gmail.com>
2024-12-31 04:12:50 -0300

    Criando entidade Candidato

--- src/main/java/com/jotahdev/job_development/modules/candidate/Controllers/CandidateController.java
+++ src/main/java/com/jotahdev/job_development/modules/candidate/Controllers/CandidateController.java
@@ -1,5 +1,20 @@
 package com.jotahdev.job_development.modules.candidate.Controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jotahdev.job_development.modules.candidate.Entities.CandidateEntity;
+
+@RestController
+@RequestMapping("/candidato")
 public class CandidateController {
-    
+
+    @PostMapping("/")
+    public void create(@RequestBody CandidateEntity candidateEntity) {
+        System.out.println("Candidato");
+        System.out.println(candidateEntity.getEmail());
+    }
+
 }
