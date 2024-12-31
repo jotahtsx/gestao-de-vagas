35af2878684bbd5da9eb18eda103bfd434a38ef9
jotahdev <joaomanoelskai@gmail.com>
2024-12-31 03:31:45 -0300

    Reinicializando projeto

--- src/main/java/com/jotahdev/job_development/JobDevelopmentApplication.java
+++ src/main/java/com/jotahdev/job_development/JobDevelopmentApplication.java
@@ -1,12 +1,12 @@
-package br.com.JobManagement.job_management;
+package com.jotahdev.job_development;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JobManagementApplication {
+public class JobDevelopmentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JobManagementApplication.class, args);
+		SpringApplication.run(JobDevelopmentApplication.class, args);
 	}
 }
--- src/test/java/com/jotahdev/job_development/JobDevelopmentApplicationTests.java
+++ src/test/java/com/jotahdev/job_development/JobDevelopmentApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.job_management.job_management;
+package com.jotahdev.job_development;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JobManagementApplicationTests {
+class JobDevelopmentApplicationTests {
 
 	@Test
 	void contextLoads() {
