9a220fd5f70237d6ebd6326a35ec58234d606efc
jotahdev <joaomanoelskai@gmail.com>
2025-01-03 16:16:34 -0300

    Criando tabela de candidato

--- src/main/java/com/jotahdev/job_development/Modules/Candidate/Entities/CandidateEntity.java
+++ src/main/java/com/jotahdev/job_development/Modules/Candidate/Entities/CandidateEntity.java
@@ -1,15 +1,26 @@
 package com.jotahdev.job_development.Modules.Candidate.Entities;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String name;
 
     @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "O nome de usuário deve conter apenas letras, números e underscores. Não é permitido o uso de espaços ou caracteres especiais, como acentos ou símbolos.")
@@ -22,4 +33,7 @@ public class CandidateEntity {
     private String password;
 
     private String resume;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
