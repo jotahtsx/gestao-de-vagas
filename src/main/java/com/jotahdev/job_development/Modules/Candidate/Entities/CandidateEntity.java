544713a7f860d2f64d576970dd6c00657b5841fe
jotahdev <joaomanoelskai@gmail.com>
2025-01-03 12:07:31 -0300

    Adicionando validações ao Entity de Candidato e Injetando no Controller

--- src/main/java/com/jotahdev/job_development/Modules/Candidate/Entities/CandidateEntity.java
+++ src/main/java/com/jotahdev/job_development/Modules/Candidate/Entities/CandidateEntity.java
@@ -2,6 +2,8 @@ package com.jotahdev.job_development.Modules.Candidate.Entities;
 
 import java.util.UUID;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,8 +11,15 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "O nome de usuário deve conter apenas letras, números e underscores. Não é permitido o uso de espaços ou caracteres especiais, como acentos ou símbolos.")
     private String username;
+
+    @Email(message = "Por favor, insira um email válido.")
     private String email;
+
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#\\$%^&\\*])(?=\\S+$).{8,16}$", message = "A senha deve ter entre 8 e 16 caracteres, incluindo pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial.")
     private String password;
+
     private String resume;
 }
