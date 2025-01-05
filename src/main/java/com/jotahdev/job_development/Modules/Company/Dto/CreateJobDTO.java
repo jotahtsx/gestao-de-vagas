6c76910c8ecb2a0a48fd53580ee7f1b9007e9cb7
jotahdev <joaomanoelskai@gmail.com>
2025-01-05 19:43:44 -0300

    Refatorando jobController

---
+++ src/main/java/com/jotahdev/job_development/Modules/Company/Dto/CreateJobDTO.java
@@ -0,0 +1,11 @@
+package com.jotahdev.job_development.Modules.Company.Dto;
+
+import lombok.Data;
+
+@Data
+public class CreateJobDTO {
+    
+    private String description;
+    private String benefits;
+    private String level;
+}
