be08dfa930e7b6c83fa08681cf2b881b95e8c680
jotahdev <joaomanoelskai@gmail.com>
2025-01-05 18:50:03 -0300

    Adicionando tempo de expiração do token

--- src/main/java/com/jotahdev/job_development/Security/SecurityConfig.java
+++ src/main/java/com/jotahdev/job_development/Security/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.jotahdev.job_development.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -17,7 +22,8 @@ public class SecurityConfig {
                         .requestMatchers("/candidato/", "/empresa/").permitAll() // Dá o acesso as duas rotas
                         .requestMatchers("/auth/empresa").permitAll()
                         .anyRequest().authenticated() // Exige autenticação para qualquer outra rota
-                );
+                )
+                .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
         return http.build();
     }
 
