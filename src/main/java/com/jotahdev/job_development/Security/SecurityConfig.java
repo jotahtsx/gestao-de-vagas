98ada7369ad86afec0e362fb4f8bf8db13fa8cc4
jotahdev <joaomanoelskai@gmail.com>
2025-01-08 07:13:30 -0300

    Criando rota de perfil de candidato pt1

--- src/main/java/com/jotahdev/job_development/Security/SecurityConfig.java
+++ src/main/java/com/jotahdev/job_development/Security/SecurityConfig.java
@@ -15,6 +15,9 @@ public class SecurityConfig {
     @Autowired
     private SecurityFilter securityFilter;
 
+    @Autowired
+    private SecurityCandidateFilter securityCandidateFilter;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
@@ -24,7 +27,8 @@ public class SecurityConfig {
                         .requestMatchers("/candidato/auth").permitAll()
                         .anyRequest().authenticated() // Exige autenticação para qualquer outra rota
                 )
-                .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
+                .addFilterBefore(securityFilter, BasicAuthenticationFilter.class)
+                .addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class);
         return http.build();
     }
 
