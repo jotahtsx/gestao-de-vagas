5f7adb63e7c53ff41f3b7e2d3045e48c610c62ad
jotahdev <joaomanoelskai@gmail.com>
2025-01-13 08:44:01 -0300

    Refatoração gerak

--- src/main/java/com/jotahdev/job_development/Exceptions/UserFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.jotahdev.job_development.Exceptions;
-
-public class UserFoundException extends RuntimeException {
-    public UserFoundException() {
-        super("Usuário já cadastrado no sistema. Verifique as informações e tente novamente.");
-    }
-}
--- src/main/java/com/jotahdev/job_development/Modules/Candidate/Dto/AuthCandidateRequestDTO.java
+++
@@ -1,4 +0,0 @@
-package com.jotahdev.job_development.Modules.Candidate.Dto;
-
-public record AuthCandidateRequestDTO(String username, String password) {
-}
